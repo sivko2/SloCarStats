cb12c129c5a3bc80eee2c406ab0ad02e2c00b1aa
Roman Verhovsek <os2warp5@gmail.com>
2023-09-17 16:14:07 +0200

    Add files via upload

--- main.go
+++ main.go
@@ -2,38 +2,42 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"sort"
 	"strings"
-	"encoding/csv"
-	"io/ioutil"
 )
 
 // Output value
 type car struct {
-	Name string
-	Count uint64
-	NewCount uint64
-	OldCount uint64
+	Name       string
+	Count      uint64
+	NewCount   uint64
+	OldCount   uint64
 	Percentage uint64
 }
 
 // Counters
 type counter struct {
-	Count uint64
-	NewCount uint64
-	OldCount uint64
+	Count      uint64
+	NewCount   uint64
+	OldCount   uint64
 	Percentage uint64
 }
 
 // Funs starts here
 func main() {
 	// Parse flags and args
-	percPtr := flag.Bool("p", false, "Ordered by percentage")
-	allPtr := flag.Bool("a", false, "Ordered by all registrations")
-	brandPtr := flag.Bool("b", false, "Grouped by brand")
+	percPtr := flag.Bool("percentage", false, "Ordered by percentage of new car registrations (default: by new car registrations)")
+	allPtr := flag.Bool("all", false, "Ordered by all car registrations (default: by new car registrations)")
+	brandPtr := flag.Bool("group", false, "Grouped by manufacturer")
+	filterPtr := flag.String("filter", "", "Filtered by year (e.g., 2023) or month + year (e.g., 082023 for Aug 2023)")
+	petrolPtr := flag.Bool("petrol", false, "Filtered by petrol engine")
+	dieselPtr := flag.Bool("diesel", false, "Filtered by diesel engine")
+	electricPtr := flag.Bool("electric", false, "Filtered by non-fuel engine")
 
 	flag.Parse()
 	args := flag.Args()
@@ -58,15 +62,19 @@ func main() {
 	modelList := []car{}
 
 	files, err := ioutil.ReadDir("stats/")
-    
+
 	if err != nil {
-        fmt.Println(err)
+		fmt.Println(err)
 		os.Exit(1)
-    }
+	}
 
 	// Read each CSV file in stats subdirectory
-    for _, file := range files {
+	for _, file := range files {
 		if file.IsDir() {
+			continue
+		}
+
+		if !strings.Contains(file.Name(), *filterPtr) {
 			continue
 		}
 
@@ -76,46 +84,59 @@ func main() {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-	
+
 		csvReader := csv.NewReader(f)
-		csvReader.Comma = ';';
-	
-		line := 0;
+		csvReader.Comma = ';'
+
+		line := 0
 		firstLine := true
-	
+
 		for {
 			// Read and parse line
 			record, err := csvReader.Read()
-	
+
 			if err != nil {
 				break
 			}
-	
+
 			if firstLine {
 				firstLine = false
 				continue
 			}
-	
+
 			dateFirstReg := record[0]
 			dateFirstRegInSlo := record[1]
 			brand := strings.Trim(strings.ToUpper(record[20]), " ")
 			category := strings.Trim(strings.ToUpper(record[33]), " ")
 			model := strings.Trim(strings.ToUpper(record[100]), " ")
-	
+			engine := strings.Trim(strings.ToUpper(record[48]), " ")
+
 			if strings.Contains(brand, "KODA") {
 				brand = "SKODA"
 			}
-	
+
 			name := brand
 
 			if !*brandPtr {
 				name += " " + model
 			}
-	
+
 			newCar := dateFirstReg == dateFirstRegInSlo
-	
+
 			// Accept cars only
 			if category != "OSEBNI AVTOMOBIL" {
+				continue
+			}
+
+			if *petrolPtr && engine != "BENCIN" {
+				continue
+			}
+
+			if *dieselPtr && engine != "DIZEL" {
+				continue
+			}
+
+			if *electricPtr && engine != "NI GORIVA" {
 				continue
 			}
 
@@ -124,19 +145,19 @@ func main() {
 					continue
 				}
 			}
-	
-			line++;
-	
+
+			line++
+
 			value, ok := modelMap[name]
-	
+
 			if !ok {
 				value = &counter{0, 0, 0, 0}
 				modelMap[name] = value
 			}
-	
+
 			value.Count = value.Count + 1
 			sum += 1
-	
+
 			if newCar {
 				value.NewCount = value.NewCount + 1
 				newSum += 1
@@ -147,13 +168,13 @@ func main() {
 
 			value.Percentage = 100 * value.NewCount / value.Count
 		}
-    }
+	}
 
 	// Make output list from map
 	for key, value := range modelMap {
 		el := car{key, value.Count, value.NewCount, value.OldCount, value.Percentage}
 		modelList = append(modelList, el)
-    }
+	}
 
 	// Sort
 	if *percPtr {
@@ -171,23 +192,23 @@ func main() {
 	}
 
 	// Print out
-	fmt.Println("+------+----------------------------------------------------+-------+-------+-------+------+")
-	fmt.Println("| #    | BRAND AND MODEL                                    | NEW   | OLD   | SUM   | PERC |")
-	fmt.Println("+------+----------------------------------------------------+-------+-------+-------+------+")
+	fmt.Println("+------+----------------------------------------------------+--------+--------+--------+------+")
+	fmt.Println("| #    | BRAND AND MODEL                                    | NEW    | OLD    | SUM    | PERC |")
+	fmt.Println("+------+----------------------------------------------------+--------+--------+--------+------+")
 
 	for i, value := range modelList {
 		if value.Count > 0 {
-			fmt.Printf("| %4d | %-50s | %5d | %5d | %5d | %3d%% |\n", i + 1, value.Name, value.NewCount, value.OldCount, value.Count, value.Percentage)
+			fmt.Printf("| %4d | %-50s | %6d | %6d | %6d | %3d%% |\n", i+1, value.Name, value.NewCount, value.OldCount, value.Count, value.Percentage)
 		}
 	}
 
 	sumPerc := 0
 
 	if sum != 0 {
-		sumPerc = 100 * newSum / sum		
+		sumPerc = 100 * newSum / sum
 	}
 
-	fmt.Println("+------+----------------------------------------------------+-------+-------+-------+------+")
-	fmt.Printf("|      | SUM                                                | %5d | %5d | %5d | %3d%% |\n", newSum, oldSum, sum, sumPerc)
-	fmt.Println("+------+----------------------------------------------------+-------+-------+-------+------+")
+	fmt.Println("+------+----------------------------------------------------+--------+--------+--------+------+")
+	fmt.Printf("|      | SUM                                                | %6d | %6d | %6d | %3d%% |\n", newSum, oldSum, sum, sumPerc)
+	fmt.Println("+------+----------------------------------------------------+--------+--------+--------+------+")
 }
